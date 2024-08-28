1183588d3c2d619524203bd99df93c2a8ac452cc
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-28 21:44:09 +0200

    feat: simplify code

--- src/Hardcoded.001/Program.cs
+++ src/Hardcoded.001/Program.cs
@@ -25,16 +25,15 @@ app.MapPost("/bot", async (TelegramBotClient bot, Update update) =>
 {
     if (update.Message is not null)
     {
-        Console.WriteLine($"Update received! {TimeProvider.System.GetUtcNow()}");
+        Console.WriteLine("Update received!");
         await ExpensiveOperation();
-        await bot.SendTextMessageAsync(update.Message.Chat, "Hard work done!");
     }
 });
 
 async Task ExpensiveOperation()
 {
     await Task.Delay(5000);
-    Console.WriteLine($"Hard work done! {TimeProvider.System.GetUtcNow()}");
+    Console.WriteLine("Hard work done!");
 }
 
 app.Run();
